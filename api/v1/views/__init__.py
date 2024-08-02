07a7adf88089ec544c859a780a3cfbf949b37e69
dr.omartalat <dr.omartalat@gmail.com>
2024-08-02 23:15:18 +0300

    from task_2 to task_4 done and ready for checking

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,14 +2,19 @@
 """
 This module initializes the views package for the AirBnB clone v3 API.
 
-It creates a Blueprint object named 'app_views' that serves as a blueprint for all the API routes.
-The Blueprint is registered with the Flask application and has a URL prefix of '/api/v1'.
+It creates a Blueprint object named 'app_views'
+that serves as a blueprint for all the API routes.
 
-The views package contains the API routes for different resources such as users, places, reviews, etc.
+The Blueprint is registered with the Flask application and has
+a URL prefix of '/api/v1'.
+
+The views package contains the API routes for different resources such as
+users, places, reviews, etc.
 """
 
 from flask import Blueprint
-# from api.v1.views.index import *
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,30 @@ This module contains the routes for the index view of the API.
 """
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    ''' routes to status page '''
-    return jsonify({'status': 'OK'})
+    """routes to status page"""
+    res = {'status': 'OK'}
+    return jsonify(res)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """routes to stats"""
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+    stats_dict = {}
+    for key, val in classes.items():
+        num = storage.count(val)
+        stats_dict[key] = num
+    return jsonify(stats_dict)
